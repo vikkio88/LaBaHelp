41b5b7ba185ea5ba688a4baf448a2e586d8de044
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2018-01-28 20:25:30 +0000

    error handling

--- src/components/ResultSections.js
+++ src/components/ResultSections.js
@@ -4,10 +4,13 @@ import {Button} from "semantic-ui-react";
 
 class ResultSections extends Component {
     renderCategories(result) {
+        if (result.filter(e => e.results.length === 0).length) {
+            return <h1>You probably pasted bullshit instead of a CV text</h1>;
+        }
         return result.map(category => (
             <div key={category.name} className="category-wrapper">
                 <h1>{category.name}</h1>
-                <ResultSet result={category.results}/>
+                <ResultSet result={category.results.sort((e, e2) => e.count > e2.count ? 1 : -1)}/>
             </div>
         ));
     }
